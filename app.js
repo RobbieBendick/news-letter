9eb4705187be581a1bd19f0d91df16f13f80a3df
mageiden <60712721+mageiden@users.noreply.github.com>
2020-06-29 06:25:51 -0700

    initial commit

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = express();
 
 
 
-app.use(express.static("public"));
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 app.get("/", function(req, res) {
 res.sendFile(__dirname + "/signup.html");
 
